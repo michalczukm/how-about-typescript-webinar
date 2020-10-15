e23fe0b370a4f98d99475bb57430789c6ee9be7e
Michal Michalczuk <michalczukm@gmail.com>
2020-10-15 16:21:04 +0200

    type List component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Todo } from '../../types';
 import { Item } from '../Item';
 
-export const List = ({ todos, actions, emptyState = null }) => {
+type Actions = {
+    deleteTodo: (id: string) => void,
+    markTodoDone: (id: string) => void,
+    markTodoNotDone: (id: string) => void
+}
+
+type Props = {
+    todos: Todo[],
+    actions: Actions,
+    emptyState: React.ReactElement<{ foo: string}>
+};
+
+export const List: React.FC<Props> = ({ todos, actions, emptyState = null }) => {
     return (
         <div>
             {todos.length <= 0 ? (
